6a84e07c8ef367a66d729f3362ae584efa4b0a88
myhkas <mmyhkas@gmail.com>
2020-06-01 19:20:07 -0700

    added Failure test

--- test/util.test.js
+++ test/util.test.js
@@ -3,7 +3,13 @@ const { add } = require("../util/util");
 describe("Add", () => {
   describe("Success", () => {
     it("should return the sum of the 2 passed arguments", () => {
-      expect(add(2, 2)).toBe(3);
+      expect(add(2, 2)).toBe(4);
+    });
+  });
+
+  describe("Failure", () => {
+    it("Should throw an error if 1st argument isn't a string", () => {
+      expect(() => add("2", 2)).toThrowError();
     });
   });
 });
--- util/util.js
+++ util/util.js
@@ -1,4 +1,5 @@
 const add = (num1, num2) => {
+  if (typeof num1 != "number") throw new Error("1st argument must be a number");
   return num1 + num2;
 };
 
