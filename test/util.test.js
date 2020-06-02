d763c25f9ab6c507f62b422b23e4bcbb94cb1211
myhkas <mmyhkas@gmail.com>
2020-06-01 19:58:42 -0700

    added error tests for minus fn

--- test/util.test.js
+++ test/util.test.js
@@ -14,7 +14,28 @@ describe("Add", () => {
     it("Should throw an error if 2nd argument isn't a string", () => {
       expect(() => add(2, "2")).toThrowError();
     });
+    it("Should throw an error if no argumentrs received", () => {
+      expect(() => add()).toThrowError();
+    });
   });
 });
 
-describe("Minus", () => {});
+describe("Minus", () => {
+  describe("Success", () => {
+    it("Should return the difference of the 2 passed arguments", () => {
+      expect(minus(3, 1)).toBe(2);
+    });
+  });
+
+  describe("Failure", () => {
+    it("Should throw an error if 1st arugment isn't a string", () => {
+      expect(() => minus("2", 2)).toThrowError();
+    });
+    it("Should throw an error if 2nd arugment isn't a string", () => {
+      expect(() => minus(2, "2")).toThrowError();
+    });
+    it("Should throw an error if no argument is passed", () => {
+      expect(() => minus()).toThrowError();
+    });
+  });
+});
--- util/util.js
+++ util/util.js
@@ -4,6 +4,10 @@ const add = (num1, num2) => {
   return num1 + num2;
 };
 
-const minus = (num1, num2) => {};
+const minus = (num1, num2) => {
+  if (typeof num1 != "number") throw new Error("1st argument must be a number");
+  if (typeof num2 != "number") throw new Error("2nd argument must be a number");
+  return num1 - num2;
+};
 
 module.exports = { add, minus };
